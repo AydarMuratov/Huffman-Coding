e9199533c078d2d2c1bd877098ba8c675f895b12
Айдар Муратов <123933904+AydarMuratov@users.noreply.github.com>
2023-05-15 11:42:58 +0500

    Add files via upload

---
+++ алгоритм Хаффмана исправленный.cpp
@@ -0,0 +1,197 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+// Структура узла дерева Хаффмана
+struct HuffmanNode {
+    char character;
+    int frequency;
+    HuffmanNode* leftChild;
+    HuffmanNode* rightChild;
+
+    HuffmanNode(char ch, int freq, HuffmanNode* left = nullptr, HuffmanNode* right = nullptr) : character(ch), frequency(freq), leftChild(left), rightChild(right) {}
+
+    ~HuffmanNode() {
+        delete leftChild;
+        delete rightChild;
+    }
+};
+
+// Функция для сравнения узлов по частоте
+struct CompareNodes {
+    bool operator()(const HuffmanNode* a, const HuffmanNode* b) {
+        return a->frequency > b->frequency;
+    }
+};
+
+// Функция для построения дерева Хаффмана
+HuffmanNode* buildHuffmanTree(unordered_map<char, int>& frequencyTable) {
+    priority_queue<HuffmanNode*, vector<HuffmanNode*>, CompareNodes> minHeap;
+
+    // Добавляем узлы в очередь
+    for (auto& item : frequencyTable) {
+        minHeap.push(new HuffmanNode(item.first, item.second));
+    }
+
+    // Строим дерево Хаффмана
+    while (minHeap.size() > 1) {
+        HuffmanNode* left = minHeap.top();
+        minHeap.pop();
+        HuffmanNode* right = minHeap.top();
+        minHeap.pop();
+        minHeap.push(new HuffmanNode('\0', left->frequency + right->frequency, left, right));
+    }
+
+    return minHeap.top();
+}
+
+// Функция для кодирования сообщения
+void encode(string inputFileName, string outputFileName) {
+    // Читаем файл в строку
+    string inputText;
+    ifstream inputFile(inputFileName);
+    if (inputFile.is_open()) {
+        char c;
+        while (inputFile.get(c)) {
+            inputText += c;
+        }
+        inputFile.close();
+    } else {
+        cout << "Unable to open input file" << endl;
+        return;
+    }
+
+    // Считаем частоту символов
+    unordered_map<char, int> frequencyTable;
+    for (char c : inputText) {
+        frequencyTable[c]++;
+    }
+
+    // Строим дерево Хаффмана
+    HuffmanNode* root = buildHuffmanTree(frequencyTable);
+
+    // Создаем таблицу кодов символов
+    unordered_map<char, string> codeTable;
+    function<void(HuffmanNode*, string)> buildCodeTable = [&](HuffmanNode* node, string code) {
+        if (node) {
+            if (node->character) {
+                codeTable[node->character] = code;
+            } else {
+                buildCodeTable(node->leftChild, code + '0');
+                buildCodeTable(node->rightChild, code + '1');
+            }
+        }
+    };
+    buildCodeTable(root, "");
+
+    // Записываем закодированный текст в выходной файл
+    ofstream outputFile(outputFileName, ios::binary);
+    if (outputFile.is_open()) {
+        string encodedText;
+        for (char c : inputText) {
+            encodedText += codeTable[c];
+        }
+        // Записываем закодированный текст в выходной файл
+        for (size_t i = 0; i < encodedText.length(); i += 8) {
+            string byte = encodedText.substr(i, 8);
+            unsigned char value = 0;
+            for (size_t j = 0; j < byte.length(); j++) {
+                if (byte[j] == '1') {
+                    value |= (1 << (7 - j));
+                }
+            }
+            outputFile.write(reinterpret_cast<const char*>(&value), sizeof(value));
+        }
+        outputFile.close();
+    } else {
+        cout << "Unable to open output file" << endl;
+    }
+
+    // Очищаем память, выделенную под дерево
+    delete root;
+}
+
+// Функция для декодирования сообщения
+void decode(string inputFileName, string outputFileName) {
+    // Читаем файл в строку
+    string encodedText;
+    ifstream inputFile(inputFileName, ios::binary);
+    if (inputFile.is_open()) {
+        char c;
+        while (inputFile.get(c)) {
+            encodedText += c;
+        }
+        inputFile.close();
+    } else {
+        cout << "Unable to open input file" << endl;
+        return;
+    }
+
+    // Считаем частоту символов
+    unordered_map<char, int> frequencyTable;
+    for (char c : encodedText) {
+        frequencyTable[c]++;
+    }
+
+    // Строим дерево Хаффмана
+    HuffmanNode* root = buildHuffmanTree(frequencyTable);
+
+    // Декодируем текст
+    string decodedText;
+    HuffmanNode* currentNode = root;
+    for (char c : encodedText) {
+        unsigned char byte = static_cast<unsigned char>(c);
+        for (int i = 7; i >= 0; i--) {
+            bool bit = ((byte >> i) & 1);
+            if (bit) {
+                currentNode = currentNode->rightChild;
+            } else {
+                currentNode = currentNode->leftChild;
+            }
+            if (currentNode->character) {
+                decodedText += currentNode->character;
+                currentNode = root;
+            }
+        }
+    }
+
+    // Записываем раскодированный текст в выходной файл
+    ofstream outputFile(outputFileName);
+    if (outputFile.is_open()) {
+        outputFile << decodedText;
+        outputFile.close();
+    } else {
+        cout << "Unable to open output file" << endl;
+    }
+
+    // Очищаем память, выделенную под дерево
+    delete root;
+}
+
+int main() {
+    cout << "Choose mode (1 - encode, 2 - decode): ";
+    int mode;
+    cin >> mode;
+
+    cout << "Enter input file path: ";
+    string inputFileName;
+    cin >> inputFileName;
+
+    cout << "Enter output file path: ";
+    string outputFileName;
+    cin >> outputFileName;
+
+    if (mode == 1) {
+        encode(inputFileName, outputFileName);
+    } else if (mode == 2) {
+        decode(inputFileName, outputFileName);
+    } else {
+        cout << "Invalid mode" << endl;
+    }
+
+    return 0;
+}
